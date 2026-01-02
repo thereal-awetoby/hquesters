9d0526484a193add75629cedefe95e168f757be0
zack <zackibi07@gmail.com>
2026-01-02 17:14:19 +0100

    add backend structure with Flask

---
+++ BackEnd/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/api/hello")
+def hello():
+    return jsonify({"message": "salam men backend"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
