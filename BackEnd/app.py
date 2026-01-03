3968eb2acd5ad0d5feed0e0864e481e55afe4e1b
zack <zackibi07@gmail.com>
2026-01-03 23:33:31 +0100

    fix bugs

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,24 +1,34 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request,Blueprint
 from google import genai
+from extension import bcrypt
 import os
+from auth_routes import auth
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 
 Api_key=os.getenv("GEMINI_API_KEY")
+
 if not Api_key:
     raise ValueError("Gemini api key not found")
 app = Flask(__name__)
-client = genai.Client(api_key="GEMINI_API_KEY")
+bcrypt.init_app(app)
+app.register_blueprint(auth)
+client = genai.Client(api_key=Api_key)
 @app.route("/api/ask",methods=["POST"])
 def ask():
     
-    question = request.json.get("What is artificial intelligence in simple terms?")
+    question = request.json.get("question")
     response = client.models.generate_content(
         model = "gemini-1.5-flash",
         contents=question
     )
+    try : 
+        response = client.models.generate_content(...)
+    except Exception as e:
+        return jsonify({"error" : "Gemini error"}),500
     return jsonify({"answer": response.text})
 @app.route("/")
 def home():
@@ -27,5 +37,6 @@ def home():
 @app.route("/api/hello")
 def hello():
     return jsonify({"message": "rak khdam"})
+
 if __name__ == "__main__":
     app.run(debug=True)
